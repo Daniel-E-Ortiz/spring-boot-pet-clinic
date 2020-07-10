9a548efc784aa5e2cadc2903798bb24329ec44aa
Daniel Ortiz <danielson.ortiz@outlook.com>
2020-07-10 09:26:51 -0500

    Added Pets and Contact info to owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInit.java
+++ pet-clinic-web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInit.java
@@ -1,6 +1,7 @@
 package com.springframework.springbootpetclinic.bootstrap;
 
 import com.springframework.springbootpetclinic.model.Owner;
+import com.springframework.springbootpetclinic.model.Pet;
 import com.springframework.springbootpetclinic.model.PetType;
 import com.springframework.springbootpetclinic.model.Vet;
 import com.springframework.springbootpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.springbootpetclinic.services.PetTypeService;
 import com.springframework.springbootpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataInit implements CommandLineRunner {
@@ -27,21 +30,43 @@ public class DataInit implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setName("Dog");
-        PetType savedDogType = petTypeService.save(dog);
+        PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("Cat");
-        PetType savedCatType = petTypeService.save(cat);
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Daniel");
         owner1.setLastName("Ortiz");
+        owner1.setAddress("123 Fakestreet");
+        owner1.setCity("Leander");
+        owner1.setTelephone("123-456-7890");
+
+        Pet danielsPet = new Pet();
+        danielsPet.setPetType(savedDogPetType);
+        danielsPet.setOwner(owner1);
+        danielsPet.setName("Stella");
+        danielsPet.setBirthDate(LocalDate.of(2016,7,5));
+
+        owner1.getPets().add(danielsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Amber");
         owner2.setLastName("Ortiz");
+        owner2.setAddress("456 Blanks Street");
+        owner2.setCity("Round Rock");
+        owner2.setTelephone("123-098-7654");
+
+        Pet ambersCat = new Pet();
+        ambersCat.setName("Derp");
+        ambersCat.setBirthDate(LocalDate.now());
+        ambersCat.setPetType(savedCatPetType);
+        ambersCat.setOwner(owner2);
+
+        owner2.getPets().add(ambersCat);
 
         ownerService.save(owner2);
 
