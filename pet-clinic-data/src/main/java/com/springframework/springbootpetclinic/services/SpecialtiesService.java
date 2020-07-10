54ec4a96d5b8f8ce452cd3c4bd721e6166500745
Daniel Ortiz <danielson.ortiz@outlook.com>
2020-07-10 11:35:00 -0500

    Added SpecialtiesMapService impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.springframework.springbootpetclinic.services;
+
+import com.springframework.springbootpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
