f25db48115a58136ff9862c86d29dfde3d1368bd
Daniel Ortiz <danielson.ortiz@outlook.com>
2020-09-23 12:22:11 -0500

    Create and implemented VisitMapService & VisitService Closes #47.

---
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.springbootpetclinic.services;
+
+import com.springframework.springbootpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
