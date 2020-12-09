076e72b70451b3da289b3efc75bbeb4ae279164e
Daniel Ortiz <danielson.ortiz@outlook.com>
2020-12-09 12:15:24 -0600

    Impl find owner. Closes #55.

--- pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/OwnerService.java
@@ -2,9 +2,12 @@ package com.springframework.springbootpetclinic.services;
 
 import com.springframework.springbootpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/springbootpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.springframework.springbootpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -78,5 +79,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .findFirst()
                 .orElse(null);
 
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //todo - impl
+        return null;
     }
 }
